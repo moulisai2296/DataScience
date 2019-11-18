0a42fa67f6ea8763cc8601e49003abf0e8b61e72
moulisai2296 <46372506+moulisai2296@users.noreply.github.com>
2019-11-18 23:55:51 +0530

    Added Comments

--- Web Scraping/web_scrape.py
+++ Web Scraping/web_scrape.py
@@ -1,20 +1,19 @@
+#Importing Libraries
 import re
 import csv
 import urllib.request as ur
 from bs4 import BeautifulSoup
 import requests
 
-
+#Opening URL of the website to scrape
 url = 'https://www.timeanddate.com/weather/india/chennai/historic'
 d = ur.urlopen(url)
+
+#Extracting all HTML code of the URL
 soup =BeautifulSoup(d,'html.parser')
+tab = soup.find('table', {'class': 'zebra tb-wt fw va-m tb-hover'}) #Finding required table
 
-tab = soup.find('table', {'class': 'zebra tb-wt fw va-m tb-hover'})
-
-print("STEP1")
-
-
-
+#Scraping Column names of a table present in the URL
 col = tab.findAll(text='Temp')
 col.append(tab.findAll(text='Weather')[0])
 col.append(tab.findAll(text='Wind')[0])
@@ -22,11 +21,7 @@ col.append(tab.findAll(text='Humidity')[0])
 col.append(tab.findAll(text='Barometer')[0])
 col.append(tab.findAll(text='Visibility')[0])
 
-
-print("STEP2")
-
-
-
+#Getting all table data and cleaning HTML tags from it
 c0 = tab.find('tbody')
 un_clean=[]
 for i in c0:
@@ -38,8 +33,6 @@ for i in c0:
 print(len(un_clean))
 print(un_clean[0])
 
-print("STEP3")
-
 
 direc = tab.findAll('span', {'class': 'comp'})
 #print(direc)
@@ -49,17 +42,15 @@ for i in direc:
 print(len(title))
 print(title[0])
 
-
-print("STEP4")
-
+#Creating a dataframe and storing the scraped table values in it.
 import pandas as pd
 df = pd.DataFrame(un_clean,columns=col)
 df.insert(3,'Direction',title)
 print(df.head(5))
 
-print("STEP5")
-
 def modify(col_name,unit,conv_unit):
+    ''' Function used to do unit conversions of different metrics and 
+        to perform some cleaning of additional spaces'''
     templist = list(df[col_name])
     l=[]
     for i in templist:
@@ -73,11 +64,10 @@ def modify(col_name,unit,conv_unit):
             string=conv_unit
             x = x + string
             my_new_list.append(x)
-
-    print(my_new_list)
+    #print(my_new_list)
     df[col_name] = my_new_list
-    print(df[col_name].head(5))
 
+#Cleaning Weather and Barometer columns
 w= list(df['Weather'])
 wl=[]
 for i in w:
@@ -90,20 +80,16 @@ for i in b:
     i= i.replace(" ", "")
     bl.append(i)
 
-modify('Temp','\xa0°F','°F')
+#Appling function on DataFrame columns
+modify('Temp','\xa0Â°F','Â°F')
 modify('Wind',' mph','mph')
-#modify('Barometer',' "Hg','"Hg')
 modify('Visibility','\xa0mi','mi')
 df['Weather'] = wl
 df['Barometer'] = bl
 print(df.head(5))
 
-print("STEP6")
-
-
+#Storing the scrapped data from Data Frame to .CSV file
 data = [df.columns.values.tolist()] + df.values.tolist()
-
 with open('data.csv','w',newline='') as f:
     w = csv.writer(f) 
     w.writerows(data) 
-
